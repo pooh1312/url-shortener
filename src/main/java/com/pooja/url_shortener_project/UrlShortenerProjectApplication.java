d893d6f8d945897481b587f08b85f1b2c65537bb
Pooja <pooja.ak147@gmail.com>
2025-09-05 22:58:46 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/pooja/url_shortener_project/UrlShortenerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pooja.url_shortener_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pooja/url_shortener_project/UrlShortenerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooja.url_shortener_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
